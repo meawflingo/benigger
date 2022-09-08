c88332caec6ddc3285384c7b5c7fe896b741c75e
meawflingo <112710771+meawflingo@users.noreply.github.com>
2022-09-08 19:15:03 +0600

    Add files via upload

---
+++ ovik's lalaland strip heaven.py
@@ -0,0 +1,10 @@
+print("Welcome to Ovik's lalaland strip heaven")
+
+age = int(input("Please enter your age: "))
+
+if age > 18:
+    print("Please come in sir, have a great night!")
+elif age < 8:
+    print("fck off kid")
+else:
+    print("Go away ngr!")
